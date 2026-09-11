fc5b35cafe969a009431e714819afe3d5dcd830d
iman Naseer <imannaseer662@gmail.com>
2026-09-11 15:00:55 +0500

    Create expense_tracker.py for tracking expenses
    Added a simple Python Expense Tracker that allows users to enter multiple expenses and calculates the total amount spent.

---
+++ expense_tracker.py
@@ -0,0 +1,24 @@
+# Expense Tracker
+
+Python Programming - Project 2
+
+## Description
+A simple Python Expense Tracker that allows users to enter multiple expenses and calculates the total amount spent.
+
+## Features
+- Enter multiple expenses
+- Automatically calculates total spent
+- Uses a loop
+- Uses an accumulator
+- Enter 0 to finish
+
+## Example
+Expenses:
+100
+50
+20
+
+Total Spent = 170
+
+## Technology
+Python
